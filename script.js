c8d7980f4ca81d7a3458e4c235b8fc564f0c7a07
Haris Kovac <hariskovac19@gmail.com>
2023-03-19 15:45:43 -0500

    Create Book constructor

---
+++ script.js
@@ -0,0 +1,12 @@
+let myLibrary = [];
+
+function Book (title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+Book.prototype.info = function () {
+  return `${title} by ${author}, ${pages}, ${read}`
+}
