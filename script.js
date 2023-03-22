550971fad6a41639b626a8aef88fcf183dde69d8
Haris Kovac <hariskovac19@gmail.com>
2023-03-22 18:58:43 -0500

    Feat: create addBookToLibrary function

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 let myLibrary = [];
 
-function Book (title, author, pages, read) {
+function Book(title, author, pages, read, image) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
+  this.image = image;
 }
 
-Book.prototype.info = function () {
+Book.prototype.info = function() {
   return `${title} by ${author}, ${pages}, ${read}`
+}
+
+function addBookToLibrary(title, author, pages, read, image) {
+  const newBook = new Book(title, author, pages, read, image);
+  myLibrary.push(newBook);
 }
