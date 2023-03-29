d051b22db92faee50824ec1a13e41888b38cd55c
Haris Kovac <hariskovac19@gmail.com>
2023-03-29 17:21:29 -0500

    Add deleteBook function to remove cards from DOM

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ let myLibrary = [];
 
 const bookshelf = document.querySelector('.bookshelf');
 const newBookButton = document.querySelector('.new-btn');
+const deleteBookButton = document.querySelector('.delete-icon')
 const cancelButton = document.querySelector('.cancel-btn');
 const formWrapper = document.querySelector('.form-wrapper');
 const form = document.querySelector('.book-form');
 const overlay = document.querySelector('.overlay');
 
 newBookButton.addEventListener('click', openBookForm);
+deleteBookButton.addEventListener('click', deleteBook);
 form.addEventListener('submit', addBookToLibrary);
 cancelButton.addEventListener('click', closeBookForm);
 
@@ -63,17 +65,26 @@ function createLastCard(element = myLibrary[myLibrary.length - 1]) {
   readStatus.classList.toggle('read-status');
   readButton.classList.toggle('read-btn');
 
+  deleteIcon.textContent = 'x';
+  deleteIcon.addEventListener('click', deleteBook);
   cover.src = element.cover;
   title.textContent = element.title;
   author.textContent = element.author;
   pages.textContent = element.pages;
   readStatus.textContent = element.read;
 
-  book.dataset.index = myLibrary.length - 1;
+  deleteIcon.dataset.title = element.title;
+  book.dataset.title = element.title;
   book.append(deleteIcon, cover, title, author, pages, readStatus, readButton);
 
   bookshelf.appendChild(book);
   checkCovers();
+}
+
+function deleteBook() {
+  const book = document.querySelector(`.book[data-title="${this.dataset.title}"]`);
+  myLibrary.splice(myLibrary.findIndex(object => object.title === this.dataset.title), 1);
+  bookshelf.removeChild(book);
 }
 
 function checkCovers() {
